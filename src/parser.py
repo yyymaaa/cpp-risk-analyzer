6ea45202f70e3b7d71ac89862ad789b5c784abda
Amy Ojuka <ojukaamy43@gmail.com>
2026-08-07 16:39:59 +0300

    Add parser.py

---
+++ src/parser.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+def extract_includes(file_path: Path) -> list[str]:
+    includes = []
+
+    with file_path.open("r", encoding="utf-8", errors="ignore") as file:
+        for line in file:
+            line = line.strip()
+
+            if line.startswith('"#include "'):
+                parts = line.split('"')
+
+                if len(parts) >= 2:
+                    includes.append(parts[1])
+    return includes
