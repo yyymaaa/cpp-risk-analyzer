faa75a7e43537125d2aab9a614c925df4a44fdd5
Amy Ojuka <ojukaamy43@gmail.com>
2026-08-08 18:46:45 +0300

    Update scanner.py

--- src/scanner.py
+++ src/scanner.py
@@ -1,11 +1,31 @@
 from pathlib import Path
 
-CPP_EXTENSIONS = {".cpp", ".cc", ".h", ".hpp"}
+CPP_EXTENSIONS = {
+    ".cpp", 
+    ".cc",
+    ".cxx",
+    ".hh", 
+    ".h", 
+    ".hpp",
+    ".hxx"
+}
 
-def scan_repository(root_dir: Path) -> list[Path]:
+def scan_repository(repository: Path) -> list[Path]:
+    found_files = [
+        path
+        for path in repository.rglob("*")
+        if path.is_file() and path.suffix.lower() in CPP_EXTENSIONS
+    ]
+
+    return sorted(found_files)
+
+"""
+
     cpp_files = []
 
     for file in root_dir.rglob("*"):
         if file.suffix in CPP_EXTENSIONS:
             cpp_files.append(file)
     return cpp_files
+
+"""
