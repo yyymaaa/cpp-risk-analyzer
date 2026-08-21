96f25dcf24bf34d5a08d91684b104ddf8760759a
Amy Ojuka <ojukaamy43@gmail.com>
2026-08-21 11:33:02 +0300

    Update graph_builder.py

--- src/graph_builder.py
+++ src/graph_builder.py
@@ -1,28 +1,39 @@
 from pathlib import Path
 import networkx as nx
 
+from parser import IncludeDirective
+from resolver import resolve_include
+
 def build_dependency_graph(
-        repository: Path,
-        files: list[Path],
-        dependencies: dict[Path, list[str]]
+    repository: Path,
+    files: list[Path],
+    dependencies: dict[Path, list[IncludeDirective]]
 ) -> nx.DiGraph:
     graph = nx.DiGraph()
     for file in files:
-        relative_path = file.relative_to(repository)
-        graph.add_node(str(relative_path))
+        relative_path = file.relative_to(repository).as_posix()
+        graph.add_node(relative_path)
 
-    known_files = {
-        file.relative_to(repository).as_posix()
-        for file in files
-    }
-
-    for file, includes in dependencies.items():
-        source = file.relative_to(repository).as_posix()
+    for source_file, includes in dependencies.items():
+        source_file.relative_to(repository).as_posix()
 
         for include in includes:
-            include_path = Path(include).as_posix()
+            result = resolve_include(
+                include,
+                source_file,
+                repository,
+            )
 
-            if include_path in known_files:
-                graph.add_edge(source, include_path)
+            if result.status == "internal":
+                target = result.target.relative_to(repository).as_posix()
+
+                graph.add_edge(
+                    source,
+                    target,
+                    relationship="inlcude",
+                    line=include.line_number,
+                    include_path=include.path,
+                    delimiter=include.delimiter,
+                )
 
     return graph
